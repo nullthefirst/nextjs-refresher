0901f81e409f4de549f8dfb4fa61f2cfd62db5cc
nullthefirst <ninte.dev@gmail.com>
2021-08-31 17:05:52 +0100

    Updated first post route

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -10,18 +10,15 @@ export default function FirstPost() {
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
-      <Image
-        src="/images/ninte-dev-profile-logo.png"
-        height={200}
-        width={200}
-        alt="Profile image"
-        title="Profile image"
-      />
-      <h2>
-        <Link href="/">
-          <a>Back to Home</a>
-        </Link>
-      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
     </Layout>
   );
 }
