de10f86ab45d13aa32daf7a477afe68664182439
nullthefirst <ninte.dev@gmail.com>
2021-08-31 16:49:20 +0100

    Added stylesheet to component

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,5 @@
+import styles from "../styles/layout.module.css";
+
 export default function Layout({ children }) {
-  return <div>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 }
