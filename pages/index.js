3d500b195e4c8ca9743dcb020cb58b1497169ade
nullthefirst <ninte.dev@gmail.com>
2021-08-31 18:29:06 +0100

    Completed posts setup on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import Link from "next/link";
+import Date from "../components/date";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
@@ -28,20 +29,22 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
-      <h1>
+      {/* <h1>
         <Link href="/posts/first-post">
           <a>&rarr; First Post</a>
         </Link>
-      </h1>
+      </h1> */}
     </Layout>
   );
 }
